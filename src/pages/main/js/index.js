4fed347ba8e76edd3abe115da1720e17824f5515
Pavel Prokopenya <pavel12minsk@mail.ru>
2023-03-07 20:35:28 +0300

    feat: add popup form

--- src/pages/main/js/index.js
+++ src/pages/main/js/index.js
@@ -2,3 +2,35 @@ import sayHi from './modules/test.js';
 
 console.log('main/index.js');
 sayHi();
+
+const modal = document.querySelector('.modal');
+const openModalBtns = document.querySelectorAll('[data-open-modal]');
+const closeModalBtns = document.querySelectorAll('[data-close-modal]');
+
+console.log(modal);
+console.log(closeModalBtns);
+
+function openModal() {
+  modal.style.display = 'block';
+}
+
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+openModalBtns.forEach(closeBtn => {
+  closeBtn.addEventListener('click', openModal);
+});
+
+closeModalBtns.forEach(closeBtn => {
+  closeBtn.addEventListener('click', closeModal);
+});
+
+modal.addEventListener('click', (e) => {
+  if (e.target === modal) {
+    console.log('modal clock!');
+    closeModal();
+  }
+});
+
+// add auto pop-up timer
