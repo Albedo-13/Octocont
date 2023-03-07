34ca1413790d0c67f3bdd2f3b112906b9c9d469e
AlinaLaniuk <alinavoronina16@gmail.com>
2023-03-07 17:18:48 +0200

    feat: add articles blocks

--- src/pages/articles/js/index.js
+++ src/pages/articles/js/index.js
@@ -1,4 +1,15 @@
 import sayHi from './modules/test.js';
 
-console.log('main/index.js');
 sayHi();
+
+const articleSection = document.querySelector('#articles-container');
+
+function addArticle(){
+  const articleTemplate = document.querySelector('#article');
+  const articleTemplateClone = articleTemplate.content.cloneNode(true);
+  articleSection.append(articleTemplateClone);
+}
+
+for(let i = 0; i < 8; i++){
+  addArticle();
+}
